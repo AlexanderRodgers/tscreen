d55353dd20d8f03f9da6ec484694d229ab9b3b0d
Alexander Rodgers <alexedrodgers@gmail.com>
2020-04-23 22:41:27 -0700

    added tooltips

--- components/Landlord/Properties.vue
+++ components/Landlord/Properties.vue
@@ -15,10 +15,42 @@
                <td>{{displayTime(app.created.seconds)}}</td>
                <td>{{app.limit}}</td>
                <td>
-                  <v-icon v-if="app.options.background">mdi-account-check</v-icon>
-                  <v-icon v-if="app.options.resume">mdi-file</v-icon>
-                  <v-icon v-if="app.options.address">mdi-home</v-icon>
-                  <v-icon v-if="app.options.credit">mdi-credit-card</v-icon>
+                  <v-tooltip top>
+                     <template v-slot:activator="{ on }">
+                        <v-icon 
+                           v-if="app.options.background"
+                           v-on="on"
+                        >mdi-account-check</v-icon>
+                     </template>
+                     <span>Background Check</span>
+                  </v-tooltip>
+                  <v-tooltip top>
+                     <template v-slot:activator="{ on }">
+                        <v-icon 
+                           v-if="app.options.resume"
+                           v-on="on"
+                        >mdi-file</v-icon>
+                     </template>
+                     <span>Resume</span>
+                  </v-tooltip>
+                  <v-tooltip top>
+                     <template v-slot:activator="{ on }">
+                        <v-icon 
+                           v-if="app.options.address" 
+                           v-on="on"
+                        >mdi-home</v-icon>
+                     </template>
+                     <span>Address History</span>
+                  </v-tooltip>
+                  <v-tooltip top>
+                     <template v-slot:activator="{ on }">
+                        <v-icon 
+                           v-if="app.options.credit"
+                           v-on="on"   
+                        >mdi-credit-card</v-icon>
+                     </template>
+                     <span>Credit History</span>
+                  </v-tooltip>
                </td>
             </tr>
          </tbody>
