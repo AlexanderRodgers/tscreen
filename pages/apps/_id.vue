962d745c8528c1d6c0e2609a25a759ab635ada83
Alexander Rodgers <alexedrodgers@gmail.com>
2020-05-06 22:35:49 -0700

    changed dates

--- pages/apps/_id.vue
+++ pages/apps/_id.vue
@@ -16,7 +16,7 @@
                <tbody>
                   <tr v-for="app in app.applicants" :key="app.id">
                      <td>Alex, Josh, Jakob, and 2 others</td>
-                     <td>Applied 5/1/2020</td>
+                     <td>Applied 5/6/2020</td>
                   </tr>
                </tbody>
             </template>
